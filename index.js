7f32e1f14a0b962a43fd7978e558e31ad310c45d
ashiqcvcv <ashiqc23@gmail.com>
2020-04-27 10:24:26 +0530

    initial Comit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const MongoClient = require('mongodb');
 const url='mongodb+srv://ashiqcv:19851055181@cluster0-lkvm8.mongodb.net/test?retryWrites=true&w=majority';
 
-const link = 'https://urliq.herokuapp.com/search/:';
+const link = 'https://urliq.herokuapp.com/search/';
 
 app.use(cors());
 app.use(bodyparser.json());
